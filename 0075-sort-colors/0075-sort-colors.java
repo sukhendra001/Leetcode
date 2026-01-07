2d3985d72c0794aee56e1c3cf78d9cdfd23d43de
Sukhendra Singh <sukhendrasingh1000@gmail.com>
2026-01-07 22:25:57 +0530

    Time: 0 ms (100%), Space: 43.8 MB (12.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -17,14 +17,23 @@ class Solution {
         // 2nd pass: overwrite array
         int index = 0;
 
-        while (count0-- > 0) {
-            nums[index++] = 0;
+        // put all 0s
+        for (int i = 0; i < count0; i++) {
+            nums[index] = 0;
+            index++;
         }
-        while (count1-- > 0) {
-            nums[index++] = 1;
+
+        // put all 1s
+        for (int i = 0; i < count1; i++) {
+            nums[index] = 1;
+            index++;
         }
-        while (count2-- > 0) {
-            nums[index++] = 2;
+
+        // put all 2s
+        for (int i = 0; i < count2; i++) {
+            nums[index] = 2;
+            index++;
         }
+
     }
 }
